7519fb9b9e30228bdacb46ae698ad3229fa9739e
bharathkamaraju <bharathkamaraja4@gmail.com>
2026-03-07 20:20:24 +0530

    assignment

---
+++ src/module4/Stringbuilder.java
@@ -0,0 +1,11 @@
+package module4;
+//program4
+
+public class Stringbuilder {
+    public static void main(String[] args) {
+        StringBuilder s= new StringBuilder("Hello");
+        System.out.println("String before concatenation :"+s);
+        s.append(" Bharath");
+        System.out.println("String after concatenation :"+s);
+    }
+}
